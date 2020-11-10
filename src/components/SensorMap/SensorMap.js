38720b453018eb9f6a2efa6ed7598b0deba93276
BoostedPenguin <60142028+BoostedPenguin@users.noreply.github.com>
2020-11-10 13:46:10 +0100

    GrapQL data getting requeried every 30 seconds

--- src/components/SensorMap/SensorMap.js
+++ src/components/SensorMap/SensorMap.js
@@ -20,7 +20,9 @@ const CELL_WIDTH = 100 / 32; // 32 cells horizontally, each one of them 100/32% 
 const CELL_HEIGHT = 100 / 14; // 14 cells vertically, each one of them 100/14% wide
 
 export const SensorMap = () => {
-  const { data, loading, error } = useQuery(GET_SENSORS);
+  const { data, loading, error } = useQuery(GET_SENSORS, {
+    pollInterval: 30000
+  });
 
   if (error) return <p>ERROR</p>;
   if (loading) return <p>Loading</p>;
