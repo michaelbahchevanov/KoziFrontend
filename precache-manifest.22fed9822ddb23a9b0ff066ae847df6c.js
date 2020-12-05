16860d813217a6e72456ab2b887fc78cc25927c4
Michael <bahchevanov.mihail@gmail.com>
2020-12-05 11:08:49 +0100

    Updates

---
+++ precache-manifest.22fed9822ddb23a9b0ff066ae847df6c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c09a93427182305cb20df918e6dc21f",
+    "url": "/kozi-frontend/index.html"
+  },
+  {
+    "revision": "f2e06bac78a854bdf5fc",
+    "url": "/kozi-frontend/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "613b4eb173cb6a498d61",
+    "url": "/kozi-frontend/static/css/main.94f935da.chunk.css"
+  },
+  {
+    "revision": "f2e06bac78a854bdf5fc",
+    "url": "/kozi-frontend/static/js/2.12abda7b.chunk.js"
+  },
+  {
+    "revision": "a73511fdd0e79fea52c5f42ad1574633",
+    "url": "/kozi-frontend/static/js/2.12abda7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "613b4eb173cb6a498d61",
+    "url": "/kozi-frontend/static/js/main.7872139c.chunk.js"
+  },
+  {
+    "revision": "c3037a1452abbaa3968e",
+    "url": "/kozi-frontend/static/js/runtime-main.25700bee.js"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/kozi-frontend/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "47d1f062ccc8aa896d56abb8fcd2b01e",
+    "url": "/kozi-frontend/static/media/map-floor-3.47d1f062.png"
+  }
+]);
