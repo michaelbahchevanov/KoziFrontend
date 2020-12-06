4fc0f1bf99653b661c26fea965c141455e9bed35
BoostedPenguin <60142028+BoostedPenguin@users.noreply.github.com>
2020-12-06 13:33:22 +0100

    Initial commit, waiting on backend for testing

--- src/components/Sensor/sensorHoverView.js
+++ src/components/Sensor/sensorHoverView.js
@@ -1,30 +1,51 @@
 import React from 'react'
 import { Overlay, Popover } from 'react-bootstrap'
 export function Sensorhoverview(props) {
-    
 
+
+  if (props.fault_code == "") {
     return (
-        <Overlay
+      <Overlay
         transition={null}
-          show={props.show}
-          target={props.target}
-          placement="top"
-          containerPadding={20}
-        >
-          <Popover id="popover-contained">
-            <Popover.Title className="text-nowrap" style={{ backgroundColor: "#349CDB", color: "#ffffff" }} as="h3">Sensor Information</Popover.Title>
-            <Popover.Content>
-              <strong>Floor:</strong> {props.floor}
-              <br />
-              <strong>X:</strong> {props.loc_x + " "}
-              <strong>Y:</strong> {props.loc_y}
-              <br />
-              <strong>T:</strong> {(Math.round(props.temperature * 100) / 100).toFixed(1) + ' °C '}
-              <strong>H:</strong> {(Math.round(props.humidity * 100) / 100).toFixed(1)}
-            </Popover.Content>
-          </Popover>
-        </Overlay>
+        show={props.show}
+        target={props.target}
+        placement="top"
+        containerPadding={20}
+      >
+        <Popover id="popover-contained">
+          <Popover.Title className="text-nowrap" style={{ backgroundColor: "#349CDB", color: "#ffffff" }} as="h3">Sensor Information</Popover.Title>
+          <Popover.Content>
+            <strong>Floor:</strong> {props.floor}
+            <br />
+            <strong>X:</strong> {props.loc_x + " "}
+            <strong>Y:</strong> {props.loc_y}
+            <br />
+            <strong>T:</strong> {(Math.round(props.temperature * 100) / 100).toFixed(1) + ' °C '}
+            <strong>H:</strong> {(Math.round(props.humidity * 100) / 100).toFixed(1)}
+          </Popover.Content>
+        </Popover>
+      </Overlay>
     )
+  }
+  else {
+    return (
+      <Overlay
+        transition={null}
+        show={props.show}
+        target={props.target}
+        placement="top"
+        containerPadding={20}
+      >
+        <Popover id="popover-contained">
+          <Popover.Title className="text-nowrap" style={{ backgroundColor: "#349CDB", color: "#ffffff" }} as="h3">Sensor Information</Popover.Title>
+          <Popover.Content>
+            <strong>ERROR CODE:</strong> {props.fault_code}
+          </Popover.Content>
+        </Popover>
+      </Overlay>
+    )
+  }
+
 }
 
 export default Sensorhoverview
--- src/components/SensorMap/SensorMap.js
+++ src/components/SensorMap/SensorMap.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { Row, Col, Container } from 'react-bootstrap';
-import { gql, useQuery } from '@apollo/client';
-import floorMapImg from './map-floor-3.png';
-import './styles.css';
-import { Sensor } from '../';
+import React from 'react'
+import { Row, Col, Container } from 'react-bootstrap'
+import { gql, useQuery } from '@apollo/client'
+import floorMapImg from './map-floor-3.png'
+import './styles.css'
+import { Sensor } from '../'
 
 const GET_SENSORS = gql`
   {
@@ -19,30 +19,31 @@ const GET_SENSORS = gql`
       loc_x
       loc_y
       floor
+      fault_code
       timestamp
     }
   }
   
-`;
+`
 
 
-const CELL_WIDTH = 100 / 32; // 32 cells horizontally, each one of them 100/32% wide
-const CELL_HEIGHT = 100 / 14; // 14 cells vertically, each one of them 100/14% wide
+const CELL_WIDTH = 100 / 32 // 32 cells horizontally, each one of them 100/32% wide
+const CELL_HEIGHT = 100 / 14 // 14 cells vertically, each one of them 100/14% wide
 
 export const SensorMap = () => {
   const { data, loading, error } = useQuery(GET_SENSORS, {
     pollInterval: 30000
-  });
+  })
 
-  if (error) return <p>ERROR</p>;
-  if (loading) return <p>Loading</p>;
-  if (!data) return <p>Not found</p>;
+  if (error) return <p>ERROR</p>
+  if (loading) return <p>Loading</p>
+  if (!data) return <p>Not found</p>
 
 
 
   let finalData = data.MeanClimateMeasurements.filter(goodData => !data.SensorFaults.some(
     faultySensor => goodData.loc_x === faultySensor.loc_x && goodData.loc_y === faultySensor.loc_y && goodData.floor === faultySensor.floor
-  ));
+  ))
 
 
   return (
@@ -62,7 +63,7 @@ export const SensorMap = () => {
               {...sensor}
               cellHeight={CELL_HEIGHT}
               cellWidth={CELL_WIDTH}
-              faulty = {true}
+              faulty={true}
             />
           ))}
 
@@ -70,5 +71,5 @@ export const SensorMap = () => {
         </Col>
       </Row>
     </Container>
-  );
-};
+  )
+}
