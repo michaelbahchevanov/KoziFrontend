d3de4cfe2f9636c4d4bc70877e2403d49c4106cc
Mihail Bondarenco <bondarenco.mihail@gmail.com>
2020-12-09 11:35:33 +0100

    feat(heatmap) add generalized parameters

--- src/components/SensorMap/SensorMap.js
+++ src/components/SensorMap/SensorMap.js
@@ -24,11 +24,13 @@ export const SensorMap = ({ workingSensors, faultySensors }) => {
         radius: 320,
         maxOpacity: .4,
         minOpacity: .0,
+        blur: 1,
         gradient: {
-          '.0': 'rgb(0,200,255)',
-          '.3': 'rgb(0,150,200)',
-          '.75': 'rgb(0,100,200)',
-          '.85': 'rgb(100,0,250)',
+          '.0': 'rgb(0,255,255)',
+          '.2': 'rgb(0,200,200)',
+          '.4': 'rgb(0,150,150)',
+          '.6': 'rgb(0,100,200)',
+          '.8': 'rgb(150,0,250)',
           '1': 'rgb(255,0,0)',
         }
       })
@@ -41,12 +43,14 @@ export const SensorMap = ({ workingSensors, faultySensors }) => {
           y: 10 + (s.loc_y - 1) * CELL_HEIGHT / 100 * container.scrollHeight,
           value: s.temperature,
         }))
+        let maxValue = Math.max(...data.map(d => d.value))
+        let minValue = Math.min(...data.map(d => d.value))
 
         console.log({ data, w: container.scrollWidth, h: container.scrollHeight, container })
 
         map.setData({
           data,
-        }).setDataMin(24).setDataMax(26)
+        }).setDataMin(minValue).setDataMax(maxValue)
 
       }
     }
