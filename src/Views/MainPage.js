9dfe2e976f63347af80321b26579530337a2b42c
Michael <bahchevanov.mihail@gmail.com>
2021-01-17 16:44:53 +0100

    deploy

--- src/Views/MainPage.js
+++ src/Views/MainPage.js
@@ -1,45 +1,61 @@
-import { gql, useQuery } from '@apollo/client'
-import React from "react"
-import { Spinner } from 'react-bootstrap'
+import React from 'react';
+import { Spinner } from 'react-bootstrap';
 
-import * as Components from "../components/index"
-import { GetSensors } from "../hooks"
+import * as Components from '../components/index';
+import { GetSensors } from '../hooks';
 
 export default function MainPage() {
+  const { data, loading, error } = GetSensors();
 
-  const { data, loading, error } = GetSensors()
+  if (error) return <h1>Something went wrong. Please try again</h1>;
 
-  if (error) return <h1>Something went wrong. Please try again</h1>
-
-  let workingSensors = []
-  let faultySensors = []
-  let maintenanceSensors = []
-
+  let workingSensors = [];
+  let faultySensors = [];
+  let maintenanceSensors = [];
 
   if (data) {
-    workingSensors = data.MeanClimateMeasurements.filter(goodData => !data.SensorFaults.some(
-      faultySensor => goodData.loc_x === faultySensor.loc_x && goodData.loc_y === faultySensor.loc_y && goodData.floor === faultySensor.floor
-    ))
+    workingSensors = data.MeanClimateMeasurements.filter(
+      (goodData) =>
+        !data.SensorFaults.some(
+          (faultySensor) =>
+            goodData.loc_x === faultySensor.loc_x &&
+            goodData.loc_y === faultySensor.loc_y &&
+            goodData.floor === faultySensor.floor
+        )
+    );
 
-    workingSensors = data.MeanClimateMeasurements.filter(goodData => !data.SensorMaintenance.some(
-      maintenanceSensor => goodData.loc_x === maintenanceSensor.loc_x && goodData.loc_y === maintenanceSensor.loc_y && goodData.floor === maintenanceSensor.floor
-    ))
+    workingSensors = data.MeanClimateMeasurements.filter(
+      (goodData) =>
+        !data.SensorMaintenance.some(
+          (maintenanceSensor) =>
+            goodData.loc_x === maintenanceSensor.loc_x &&
+            goodData.loc_y === maintenanceSensor.loc_y &&
+            goodData.floor === maintenanceSensor.floor
+        )
+    );
 
-    maintenanceSensors = data.SensorMaintenance
+    maintenanceSensors = data.SensorMaintenance;
 
-    faultySensors = data.SensorFaults
+    faultySensors = data.SensorFaults;
   }
 
   return (
     <div>
-
       {/* Loading spinner */}
-      {loading && <div className="d-flex">
-        <Spinner className="mx-auto" animation="border" role="status"> </Spinner>
-      </div>}
+      {loading && (
+        <div className='d-flex'>
+          <Spinner className='mx-auto' animation='border' role='status'>
+            {' '}
+          </Spinner>
+        </div>
+      )}
 
-      <Components.SensorMap workingSensors={workingSensors} faultySensors={faultySensors} maintenanceSensors={maintenanceSensors} />
+      <Components.SensorMap
+        workingSensors={workingSensors}
+        faultySensors={faultySensors}
+        maintenanceSensors={maintenanceSensors}
+      />
       <Components.Widgets workingSensors={workingSensors} />
     </div>
-  )
+  );
 }
--- src/components/SensorMap/SensorMap.js
+++ src/components/SensorMap/SensorMap.js
@@ -1,66 +1,96 @@
-import React, { useState, useLayoutEffect } from 'react'
-import { Row, Col, Container, Button, ToggleButton, ToggleButtonGroup } from 'react-bootstrap'
-import './styles.css'
-import { Sensor } from '../'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFire } from '@fortawesome/free-solid-svg-icons'
-import { useToggleState } from '../../hooks/useToggleState'
-import { Heatmap } from '../Heatmap/Heatmap'
-import { getFloorConfig } from './floorConfigs'
+import React, { useState } from 'react';
+import {
+  Row,
+  Col,
+  Container,
+  Button,
+  ToggleButton,
+  ToggleButtonGroup,
+} from 'react-bootstrap';
+import './styles.css';
+import { Sensor } from '../';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFire } from '@fortawesome/free-solid-svg-icons';
+import { useToggleState } from '../../hooks/useToggleState';
+import { Heatmap } from '../Heatmap/Heatmap';
+import { getFloorConfig } from './floorConfigs';
 
-const LOCAL_KEY_SHOW_HEATMAP = 'PREFERENCES_SHOW_HEATMAP'
+const LOCAL_KEY_SHOW_HEATMAP = 'PREFERENCES_SHOW_HEATMAP';
 
-export const SensorMap = ({ workingSensors, faultySensors, maintenanceSensors }) => {
+export const SensorMap = ({
+  workingSensors,
+  faultySensors,
+  maintenanceSensors,
+}) => {
+  const heatmapPrefence =
+    localStorage.getItem(LOCAL_KEY_SHOW_HEATMAP) !== 'false';
 
-  const heatmapPrefence = localStorage.getItem(LOCAL_KEY_SHOW_HEATMAP) !== "false"
+  let [showHeatMap, toggleHeatmap] = useToggleState(heatmapPrefence);
+  let [displayFloor, setDisplayFloor] = useState(null);
 
-  let [showHeatMap, toggleHeatmap] = useToggleState(heatmapPrefence)
-  let [displayFloor, setDisplayFloor] = useState(null)
+  let allfloors = [...workingSensors, ...faultySensors, ...maintenanceSensors]
+    .map((sensor) => sensor.floor)
+    .sort((a, b) => Math.sign(a - b));
+  let floors = new Set(allfloors);
 
-  let allfloors = [...workingSensors, ...faultySensors, ...maintenanceSensors].map(sensor => sensor.floor).sort((a, b) => Math.sign(a - b))
-  let floors = new Set(allfloors)
+  if (displayFloor === null && allfloors[0] !== undefined)
+    setDisplayFloor(allfloors[0]);
 
-  if (displayFloor === null && (allfloors[0] !== undefined))
-    setDisplayFloor(allfloors[0])
+  let floorConfig = getFloorConfig(displayFloor);
 
-  let floorConfig = getFloorConfig(displayFloor)
+  const cellWidth = 100 / floorConfig.dimX; // 32 cells horizontally, each one of them 100/32% wide
+  const cellHeight = 100 / floorConfig.dimY; // 14 cells vertically, each one of them 100/14% wide
 
-  const cellWidth = 100 / floorConfig.dimX // 32 cells horizontally, each one of them 100/32% wide
-  const cellHeight = 100 / floorConfig.dimY // 14 cells vertically, each one of them 100/14% wide
+  workingSensors = workingSensors.filter(
+    (sensor) => sensor.floor === displayFloor
+  );
+  faultySensors = faultySensors.filter(
+    (sensor) => sensor.floor === displayFloor
+  );
+  maintenanceSensors = maintenanceSensors.filter(
+    (sensor) => sensor.floor === displayFloor
+  );
 
-  workingSensors = workingSensors.filter(sensor => sensor.floor === displayFloor)
-  faultySensors = faultySensors.filter(sensor => sensor.floor === displayFloor)
-  maintenanceSensors = maintenanceSensors.filter(sensor => sensor.floor === displayFloor)
-
-  let heatmapDatapoints = workingSensors.map(s => ({
+  let heatmapDatapoints = workingSensors.map((s) => ({
     xPercent: (s.loc_x - 1) * cellWidth,
     yPercent: (s.loc_y - 1) * cellHeight,
     value: s.temperature,
-  }))
+  }));
 
   const toggleHeatmapClick = () => {
-    localStorage.setItem(LOCAL_KEY_SHOW_HEATMAP, !showHeatMap)
-    toggleHeatmap()
-  }
+    localStorage.setItem(LOCAL_KEY_SHOW_HEATMAP, !showHeatMap);
+    toggleHeatmap();
+  };
 
   return (
     <>
       <Container fluid>
-
         {/* Buttons */}
-        <Row className='mt-n4 pb-3' >
-          <Col xs={1} ls={2}>
-          </Col>
-          <Col xs="auto">
-            <Button variant="outline-primary" onClick={toggleHeatmapClick}>
-              <FontAwesomeIcon icon={faFire} size="2x" className="pr-1" />
+        <Row className='mt-n4 pb-3'>
+          <Col xs={1} ls={2}></Col>
+          <Col xs='auto'>
+            <Button variant='outline-primary' onClick={toggleHeatmapClick}>
+              <FontAwesomeIcon icon={faFire} size='2x' className='pr-1' />
               <span>Heatmap</span>
             </Button>
           </Col>
           <Col>
-            <ToggleButtonGroup className="h-100" value={displayFloor} onChange={setDisplayFloor} name="floorSelectionGroup">
-              {[...floors].map(floor => (
-                <ToggleButton value={floor} variant="outline-primary" className="d-flex align-items-center" key={floor}> {floor} </ToggleButton>
+            <ToggleButtonGroup
+              className='h-100'
+              value={displayFloor}
+              onChange={setDisplayFloor}
+              name='floorSelectionGroup'
+            >
+              {[...floors].map((floor) => (
+                <ToggleButton
+                  value={floor}
+                  variant='outline-primary'
+                  className='d-flex align-items-center'
+                  key={floor}
+                >
+                  {' '}
+                  {floor}{' '}
+                </ToggleButton>
               ))}
             </ToggleButtonGroup>
           </Col>
@@ -69,13 +99,14 @@ export const SensorMap = ({ workingSensors, faultySensors, maintenanceSensors })
         {/* Sensor Map */}
         <Row className='justify-content-center'>
           <Col xs={0} lg={1}></Col>
-          <Col className='p-0' >
+          <Col className='p-0'>
             {/* Heatmap */}
             {showHeatMap && <Heatmap data={heatmapDatapoints} />}
 
             {/* Working sensors */}
             {workingSensors.map((sensor) => (
-              <Sensor key={[sensor.loc_x, sensor.loc_y, sensor.floor].join("-")}
+              <Sensor
+                key={[sensor.loc_x, sensor.loc_y, sensor.floor].join('-')}
                 {...sensor}
                 cellHeight={cellHeight}
                 cellWidth={cellWidth}
@@ -84,7 +115,8 @@ export const SensorMap = ({ workingSensors, faultySensors, maintenanceSensors })
 
             {/* Faulty sensors */}
             {faultySensors.map((sensor) => (
-              <Sensor key={[sensor.loc_x, sensor.loc_y, sensor.floor].join("-")}
+              <Sensor
+                key={[sensor.loc_x, sensor.loc_y, sensor.floor].join('-')}
                 {...sensor}
                 cellHeight={cellHeight}
                 cellWidth={cellWidth}
@@ -92,10 +124,10 @@ export const SensorMap = ({ workingSensors, faultySensors, maintenanceSensors })
               />
             ))}
 
-
             {/* Maintenance sensors */}
             {maintenanceSensors.map((sensor) => (
-              <Sensor key={[sensor.loc_x, sensor.loc_y, sensor.floor].join("-")}
+              <Sensor
+                key={[sensor.loc_x, sensor.loc_y, sensor.floor].join('-')}
                 {...sensor}
                 cellHeight={cellHeight}
                 cellWidth={cellWidth}
@@ -103,13 +135,17 @@ export const SensorMap = ({ workingSensors, faultySensors, maintenanceSensors })
               />
             ))}
 
-            <img className='w-100 floor-map' id="wp" src={floorConfig.map} alt='Map of the floor' />
+            <img
+              className='w-100 floor-map'
+              id='wp'
+              src={floorConfig.map}
+              alt='Map of the floor'
+            />
           </Col>
           <Col xs={0} lg={1}></Col>
-
         </Row>
       </Container>
       <div></div>
     </>
-  )
-}
+  );
+};
